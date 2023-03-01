57286edfd30f0311f1fc035a4a405f7db4206aa5
Kiss Csongor <kiss.csongor2002@gmail.com>
2023-03-01 20:37:48 +0100

    Képek, login, főoldal, mappák

--- frontend/fooldal.php
+++ frontend/fooldal.php
@@ -38,6 +38,10 @@
           </form>
         </li>
       </ul>
+
+      <a class="navbar-brand" href="../server/belepes.php">
+        <img src="../kepek/profilKepek/defaultAvatar.svg" alt="Avatar Logo" style="width:40px;" class="rounded-pill"> 
+      </a>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
