fb9df930ceeb1c2d0f09f82357f2cb76d42a33ec
Kiss Csongor <72169787893@szily.hu>
2023-03-08 13:27:47 +0100

    szuro php

---
+++ server/szures.php
@@ -0,0 +1,12 @@
+<?php
+    class Szures{
+    public mysqli $csatlakozas;
+    function __construct()
+    {
+        $this->csatlakozas = new mysqli("localhost","root","","turazas");
+    }
+    function SzuroRendszer(){
+        $adat = $this->query("SELECT * FROM megye WHERE turak_szama = ".$chck."");
+    }
+    }
+?>
