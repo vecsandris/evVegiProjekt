03c6bba841b7c8f4d9ae4345621001a3962343f8
Kiss Csongor <72169787893@szily.hu>
2023-03-08 13:57:04 +0100

    filter

--- frontend/fooldal.php
+++ frontend/fooldal.php
@@ -47,6 +47,9 @@
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
+      <form action="" method="post">
+      <i class="fa-solid fa-filters"></i>
+      </form>
     </div>
   </div>
 </nav>
